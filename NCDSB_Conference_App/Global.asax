829a170ab41c4fa129c97be9bc90337ad00f5351
Nikita Ulybin <ardernord@gmail.com>
2018-02-25 18:56:10 -0500

    Add project files.

---
+++ NCDSB_Conference_App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NCDSB_Conference_App.MvcApplication" Language="C#" %>
---
+++ NCDSB_Conference_App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NCDSB_Conference_App.Startup))]
+namespace NCDSB_Conference_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
