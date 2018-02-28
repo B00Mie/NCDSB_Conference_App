3f0edd7386990b63a19b2528ae21e7e569767e4b
Ardernord <ardernord@yahoo.com>
2018-02-27 19:34:45 -0500

    Models: Conference + Lookup, Mileage

---
+++ NCDSB_Conference_App/Models/Conference.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NCDSB_Conference_App.Models
+{
+    public class Conference
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Conference Name")]
+        [Required(ErrorMessage = "This field is required")]
+        public string Name { get; set; }
+
+        [Display(Name = "Conference Start Date")]
+        [DataType(DataType.DateTime)]
+        [Required(ErrorMessage = "This field is required")]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "Conference End Date")]
+        [DataType(DataType.DateTime)]
+        [Required(ErrorMessage = "This field is required")]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Conference Location")]
+        [Required(ErrorMessage = "This field is required")]
+        public string Location { get; set; }
+
+        [Display(Name = "Conference Description")]
+        [Required(ErrorMessage = "This field is required")]
+        public string Description { get; set; }
+
+        public virtual ICollection<Mileage> Mileages { get; set; }
+    }
+
+    public class Conference_Cost_Lookup
+    {
+        [ForeignKey("Conference")]
+        public int ID { get; set; }
+
+        [Display(Name = "Registration Cost")]
+        [Required(ErrorMessage = "This field is required")]
+        [DataType(DataType.Currency)]
+        public decimal RegistrationCost { get; set; }
+
+        [Display(Name = "Meal Cost")]
+        [Required(ErrorMessage = "This field is required")]
+        [DataType(DataType.Currency)]
+        public decimal MealCost { get; set; }
+
+        [Display(Name = "Accomodation Cost")]
+        [Required(ErrorMessage = "This field is required")]
+        [DataType(DataType.Currency)]
+        public decimal AccomodationCost { get; set; }
+
+        [Display(Name = "Other Cost")]
+        [Required(ErrorMessage = "This field is required")]
+        [DataType(DataType.Currency)]
+        public decimal OtherCost { get; set; }
+
+        public decimal TotalCost
+        {
+            get
+            {
+                return RegistrationCost + MealCost + AccomodationCost + OtherCost;
+            }
+        }
+
+        public virtual Conference Conference { get; set; }
+
+    }
+}
---
+++ NCDSB_Conference_App/Models/Mileage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NCDSB_Conference_App.Models
+{
+    public class Mileage
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Estimated Cost")]
+        [Required(ErrorMessage = "This field is required")]
+        [DataType(DataType.Currency)]
+        public decimal EstimatedCost { get; set; }
+
+        [Display(Name = "Actual Cost")]
+        [DataType(DataType.Currency)]
+        public decimal ActualCost { get; set; }
+
+        public virtual Conference Conference { get; set; }
+
+        [Display(Name = "Confirmation receipt")]
+        public int ReceiptImageID{ get; set; }
+
+        [Display(Name = "Applicant")]
+        public int UserID { get; set;}
+
+        [Display(Name = "Conference")]
+        public int ConferenceID { get; set; }
+    }
+}
